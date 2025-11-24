c05dffdab80e77c4d3e1ea7548ed7e5888281594
marc226 <ge25yuf@tum.de>
2025-11-24 21:35:07 +0100

    update run envoy script: os indpendent

--- run_envoy.sh
+++ run_envoy.sh
@@ -1,1 +1,6 @@
-sudo docker run --rm --name envoy-grpc-proxy -p 8080:8080 -v $(pwd)/envoy.yaml:/etc/envoy/envoy.yaml --network host envoyproxy/envoy:v1.28-latest
+docker run --rm \
+  --name envoy-grpc-proxy \
+  --add-host=host.docker.internal:host-gateway \
+  -p 8080:8080 \
+  -v "$(pwd)/envoy.yaml:/etc/envoy/envoy.yaml" \
+  envoyproxy/envoy:v1.28-latest
